95f11c0ac2608a336c3361620a25d4eb9b8bc920
Ademir Matheus <ademirmatheusoficial@gmail.com>
2025-10-17 17:53:07 -0300

    Modelagem do banco de dados, NinjaModel, MissoesModel Concluidos, anotations feitas para relacionamento de tabelas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -0,0 +1,46 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missao")
+public class MissaoModel {
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dificuldade;
+
+    // @OneToMany uma missao pode ter varios ninjas;
+    @OneToMany(mappedBy = "missao")
+    private List<NinjaModel> ninjas;
+
+    public MissaoModel() {
+    }
+
+    public MissaoModel(String nome, String dificuldade, List<NinjaModel> ninjas) {
+        this.nome = nome;
+        this.dificuldade = dificuldade;
+        this.ninjas = ninjas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDificuldade() {
+        return dificuldade;
+    }
+
+    public void setDificuldade(String dificuldade) {
+        this.dificuldade = dificuldade;
+    }
+}
