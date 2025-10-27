fd16cc67c1df868be4b595c68b8f97e96a5ef228
Ademir Matheus <ademirmatheusoficial@gmail.com>
2025-10-27 19:43:05 -0300

    feat: Implementa endpoints PostMapping, GetMapping, PutMapping, DeleteMapping anotations (CRUD) no MissaoController.

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,4 +1,25 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missao")
 public class MissaoController {
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "criou missao";
+    }
+    @GetMapping("/listar")
+    public String mostrarMissao() {
+        return "Mostrar missoes";
+    }
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "alterou missao";
+    }
+    @DeleteMapping("/deletar")
+    public String deletarMissao(){
+        return "Deletou missao";
+    }
 }
