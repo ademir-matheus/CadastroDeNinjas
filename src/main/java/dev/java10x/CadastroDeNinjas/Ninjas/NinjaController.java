9a9ee2e2ba2bb6d8ad16ba8fb4606b102536e296
Ademir Matheus <ademirmatheusoficial@gmail.com>
2025-10-27 19:18:12 -0300

    feat: Implementa endpoints PostMapping, GetMapping, PutMapping, DeleteMapping anotations (CRUD) no ninjaController.

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,36 @@ public class NinjaController {
     @GetMapping("/boasVindas")
     public String boasVindas() {
         return "Minha primeira mensagem nessa rota";
+    }
+
+    // Create
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    // read
+    @GetMapping("/todos")
+    public String mostrarNinjas() {
+        return "Mostrar ninjas";
+    }
+
+    // read
+    @GetMapping("/todosid")
+    public String mostrarNinjaId(){
+        return "Mostrar ninjas";
+    }
+
+    // update
+    @PutMapping("alterar")
+    public String alterarNinja() {
+        return "alterar ninja";
+    }
+
+    // delete
+    @DeleteMapping("/deletar")
+    public String deletarNinja() {
+        return "deleta ninja";
     }
 
 }
